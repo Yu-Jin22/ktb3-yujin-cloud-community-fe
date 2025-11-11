10df63acdb8b23a947c0e178b7cc2a80779b78e8
Yu-jin22 <hanyujin9353@gmail.com>
2025-11-11 17:24:34 +0900

    [Express 프록시(api.js) 제거 및 백엔드 직접 호출 구조로 전환] - /api 라우터 연결 제거 - 모든 요청 URL을 http://localhost:8080 기준으로 직접 전달하도록 변경 - 타임리프 페이지 경로 수정

--- public/js/common/apiFetch.js
+++ public/js/common/apiFetch.js
@@ -1,3 +1,4 @@
+const BACKEND_URL = "http://localhost:8080";
 export async function apiFetch(url, options = {}) {
   try {
     const fetchOptions = {
@@ -12,13 +13,28 @@ export async function apiFetch(url, options = {}) {
         fetchOptions.headers["Content-Type"] || "application/json";
     }
 
-    const res = await fetch(url, fetchOptions);
+    const res = await fetch(BACKEND_URL + url, fetchOptions);
 
     // 인증 만료 감지
+
     if (res.status === 401) {
-      alert("로그인이 만료되었습니다. 다시 로그인 해주세요.");
-      location.href = "/login";
-      return null;
+      console.warn("Access Token 만료 → refresh 시도");
+
+      // refreshToken은 HttpOnly 쿠키로 자동 전송됨
+      const refreshRes = await fetch(BACKEND_URL + "/api/auth/refresh", {
+        method: "POST",
+        credentials: "include",
+      });
+
+      // refresh 실패 → 로그인 만료
+      if (!refreshRes.ok) {
+        alert("로그인이 만료되었습니다. 다시 로그인해주세요.");
+        window.location.href = "/login";
+        return null;
+      }
+
+      // refresh 성공 → 새 accessToken 쿠키 적용됨 → 원래 요청 재시도
+      res = await fetch(BACKEND_URL + url, fetchOptions);
     }
 
     // 서버 에러 등 일반 오류 처리
